e98cb7029489068f2370e2634001d702aaf2ed6a
Madison Lester <madiali@ad.unc.edu>
2023-04-06 11:43:21 -0400

    WIP

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-import express from 'express'
-import rpsls from 'a03-madiali'
+import express from 'express';
+import minimist from 'minimist';
+import { rps, rpsls } from './lib/a03-madiali/lib/rpsls.js';
+
+const app = express();
+var argv = minimist(process.argv.slice(2));
+const PORT = argv.port ? argv.port : 5000;
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+});
+
+app.get('/app', (req, res) => {
+    res.send('200 OK');
+})
+
+app.get('/app/rps', (req, res) => {
+    res.send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+    res.send(rpsls());
+})
+
+app.get('*', (req, res) => {
+    res.send('404 NOT FOUND');
+})
