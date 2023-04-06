5c035b4bbcede8af85f6807023046720d68f1207
Madison Lester <madiali@ad.unc.edu>
2023-04-06 12:06:44 -0400

    Progress

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import minimist from 'minimist';
 import { rps, rpsls } from './lib/a03-madiali/lib/rpsls.js';
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 var argv = minimist(process.argv.slice(2));
 const PORT = argv.port ? argv.port : 5000;
 
@@ -20,6 +23,13 @@ app.get('/app/rps', (req, res) => {
 
 app.get('/app/rpsls', (req, res) => {
     res.send(rpsls());
+})
+
+app.get('/app/rps/play', (req, res) => {
+    let shot = req.body.shot;
+    if (shot) {
+        res.send(rps(shot))
+    }
 })
 
 app.get('*', (req, res) => {
